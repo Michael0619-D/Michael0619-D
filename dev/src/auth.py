df034bf6316596e5a8377f2a652006f38236726e
Michael0619-D <michaeldrew0619@gmail.com>
2023-01-09 17:15:32 +0500

    Patch OAuth callback edge case

--- dev/src/auth.py
+++ dev/src/auth.py
@@ -2,3 +2,4 @@
 # 2023-01-02T18:06:11  Fix session timeout handling  (rev 3)
 # 2023-01-03T19:14:20  Patch OAuth callback edge case  (rev 4)
 # 2023-01-09T11:15:52  Handle expired tokens on refresh  (rev 13)
+# 2023-01-09T17:15:32  Patch OAuth callback edge case  (rev 14)
