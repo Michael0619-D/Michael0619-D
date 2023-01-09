23e73d9d7c4dd85c7570878b7ee41cc2999904ae
Michael0619-D <michaeldrew0619@gmail.com>
2023-01-09 11:15:52 +0500

    Handle expired tokens on refresh

--- dev/src/auth.py
+++ dev/src/auth.py
@@ -1,3 +1,4 @@
 """Authentication helpers."""
 # 2023-01-02T18:06:11  Fix session timeout handling  (rev 3)
 # 2023-01-03T19:14:20  Patch OAuth callback edge case  (rev 4)
+# 2023-01-09T11:15:52  Handle expired tokens on refresh  (rev 13)
