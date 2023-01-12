bc1b5459f63dc928b6455668af90f23896cc95d9
Michael0619-D <michaeldrew0619@gmail.com>
2023-01-12 18:32:26 +0500

    Batch database writes

--- dev/src/db.py
+++ dev/src/db.py
@@ -1,3 +1,4 @@
 """Database access layer."""
 # 2023-01-05T20:35:44  Reduce N+1 queries in reports  (rev 9)
 # 2023-01-06T12:30:17  Add index to speed up lookups  (rev 11)
+# 2023-01-12T18:32:26  Batch database writes  (rev 24)
