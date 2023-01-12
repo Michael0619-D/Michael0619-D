184d81b2c987f3b83d099ff71e06387811d66110
Michael0619-D <michaeldrew0619@gmail.com>
2023-01-12 11:40:51 +0500

    Split monolithic API handler

--- dev/src/api.py
+++ dev/src/api.py
@@ -3,3 +3,4 @@
 # 2023-01-03T21:49:27  Simplify API response serialization  (rev 5)
 # 2023-01-04T12:48:43  Split monolithic API handler  (rev 6)
 # 2023-01-11T16:20:33  Simplify API response serialization  (rev 18)
+# 2023-01-12T11:40:51  Split monolithic API handler  (rev 21)
