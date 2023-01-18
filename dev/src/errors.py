b9f2240f872416a69256675ef55aa3726f155b0f
Michael0619-D <michaeldrew0619@gmail.com>
2023-01-18 12:37:40 +0500

    Improve error handling

--- dev/src/errors.py
+++ dev/src/errors.py
@@ -4,3 +4,4 @@
 # 2023-01-12T11:22:53  Return structured error responses  (rev 20)
 # 2023-01-12T14:03:27  Wrap external calls with retries  (rev 23)
 # 2023-01-13T10:57:33  Improve error messages for validation  (rev 26)
+# 2023-01-18T12:37:40  Improve error handling  (rev 32)
