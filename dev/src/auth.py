31c6c239583f287fe7f4557d796d3ba0fef8477e
Michael0619-D <michaeldrew0619@gmail.com>
2023-01-19 11:38:56 +0500

    Fix authentication bug

--- dev/src/auth.py
+++ dev/src/auth.py
@@ -8,3 +8,4 @@
 # 2023-01-13T13:37:01  Fix authentication bug  (rev 27)
 # 2023-01-18T15:26:45  Fix session timeout handling  (rev 33)
 # 2023-01-18T20:44:47  Fix authentication bug in login flow  (rev 37)
+# 2023-01-19T11:38:56  Fix authentication bug  (rev 40)
