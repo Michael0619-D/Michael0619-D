45522c320e87d2f3eef92b1e29acbf487cd90a68
Michael0619-D <michaeldrew0619@gmail.com>
2023-01-19 13:01:48 +0500

    Split monolithic API handler

--- dev/src/api.py
+++ dev/src/api.py
@@ -6,3 +6,4 @@
 # 2023-01-12T11:40:51  Split monolithic API handler  (rev 21)
 # 2023-01-15T13:57:04  Refactor API handlers  (rev 28)
 # 2023-01-18T17:17:58  Split monolithic API handler  (rev 35)
+# 2023-01-19T13:01:48  Split monolithic API handler  (rev 41)
