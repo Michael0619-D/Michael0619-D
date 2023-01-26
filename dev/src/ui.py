c33e353f997337d63a0b445b25be6faf47c87a5e
Michael0619-D <michaeldrew0619@gmail.com>
2023-01-26 21:39:08 +0500

    Debounce search input

--- dev/src/ui.py
+++ dev/src/ui.py
@@ -3,3 +3,4 @@
 # 2023-01-10T15:41:18  Improve mobile responsiveness  (rev 16)
 # 2023-01-18T12:20:14  Polish loading states  (rev 31)
 # 2023-01-24T16:07:04  Debounce search input  (rev 51)
+# 2023-01-26T21:39:08  Debounce search input  (rev 56)
