c08447b7842893007725d8fd13c81687e038db52
Michael0619-D <michaeldrew0619@gmail.com>
2023-02-01 16:12:14 +0500

    Stabilize flaky integration test

--- dev/tests/test_suite.py
+++ dev/tests/test_suite.py
@@ -4,3 +4,4 @@
 # 2023-01-19T09:21:59  Add unit tests  (rev 38)
 # 2023-01-25T10:07:44  Add unit tests  (rev 52)
 # 2023-01-31T16:05:31  Add unit tests  (rev 61)
+# 2023-02-01T16:12:14  Stabilize flaky integration test  (rev 65)
