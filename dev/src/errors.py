91b0ef0ebcb3b945a42fca1db03b0019aab485a7
Michael0619-D <michaeldrew0619@gmail.com>
2023-02-24 16:17:40 +0500

    Wrap external calls with retries

--- dev/src/errors.py
+++ dev/src/errors.py
@@ -14,3 +14,4 @@
 # 2023-02-20T18:02:59  Improve error handling  (rev 101)
 # 2023-02-23T19:38:00  Wrap external calls with retries  (rev 109)
 # 2023-02-24T10:39:14  Improve error messages for validation  (rev 113)
+# 2023-02-24T16:17:40  Wrap external calls with retries  (rev 114)
