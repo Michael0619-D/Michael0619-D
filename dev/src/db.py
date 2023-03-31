ec048c45650c3646c24a0bd237b8d604604d7c9a
Michael0619-D <michaeldrew0619@gmail.com>
2023-03-31 09:28:26 +0500

    Batch database writes

--- dev/src/db.py
+++ dev/src/db.py
@@ -24,3 +24,4 @@
 # 2023-03-23T18:48:53  Reduce N+1 queries in reports  (rev 175)
 # 2023-03-27T21:29:35  Reduce N+1 queries in reports  (rev 184)
 # 2023-03-29T17:53:12  Reduce N+1 queries in reports  (rev 189)
+# 2023-03-31T09:28:26  Batch database writes  (rev 192)
