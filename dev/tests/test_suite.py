28d613bb455cfa8755e143383c920ba0b1afc336
Michael0619-D <michaeldrew0619@gmail.com>
2023-04-19 09:49:22 +0500

    Add regression test for auth flow

--- dev/tests/test_suite.py
+++ dev/tests/test_suite.py
@@ -23,3 +23,4 @@
 # 2023-04-10T15:37:32  Stabilize flaky integration test  (rev 210)
 # 2023-04-12T19:22:31  Add unit tests  (rev 217)
 # 2023-04-16T14:08:33  Increase test coverage for API layer  (rev 223)
+# 2023-04-19T09:49:22  Add regression test for auth flow  (rev 226)
