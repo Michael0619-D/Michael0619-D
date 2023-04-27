fb6d110d41d7b147fa772951e6be59ca102fd164
Michael0619-D <michaeldrew0619@gmail.com>
2023-04-27 15:20:05 +0500

    Batch database writes

--- dev/src/db.py
+++ dev/src/db.py
@@ -29,3 +29,4 @@
 # 2023-04-13T18:33:19  Add index to speed up lookups  (rev 219)
 # 2023-04-14T10:16:49  Cache frequent read queries  (rev 220)
 # 2023-04-26T16:44:51  Reduce N+1 queries in reports  (rev 234)
+# 2023-04-27T15:20:05  Batch database writes  (rev 239)
