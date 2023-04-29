9e0e40752afc43c5c64db6820610325bd76bdcc3
Michael0619-D <michaeldrew0619@gmail.com>
2023-04-29 18:13:46 +0500

    Add regression test for auth flow

--- dev/tests/test_suite.py
+++ dev/tests/test_suite.py
@@ -26,3 +26,4 @@
 # 2023-04-19T09:49:22  Add regression test for auth flow  (rev 226)
 # 2023-04-20T21:15:07  Add unit tests  (rev 228)
 # 2023-04-27T11:49:32  Add regression test for auth flow  (rev 237)
+# 2023-04-29T18:13:46  Add regression test for auth flow  (rev 246)
