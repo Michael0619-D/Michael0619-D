18c03c8cea95f4596a790e1fcfa1206d90ee9a81
Michael0619-D <michaeldrew0619@gmail.com>
2023-05-22 09:26:21 +0500

    Debounce search input

--- dev/src/ui.py
+++ dev/src/ui.py
@@ -35,3 +35,4 @@
 # 2023-05-18T19:38:50  Improve UI responsiveness  (rev 260)
 # 2023-05-19T15:45:56  Fix layout shift on load  (rev 262)
 # 2023-05-19T17:23:05  Improve mobile responsiveness  (rev 263)
+# 2023-05-22T09:26:21  Debounce search input  (rev 264)
