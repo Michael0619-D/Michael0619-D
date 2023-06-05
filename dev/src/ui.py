20fd0c678e950e65542408e65841c08f7e50bbb8
Michael0619-D <michaeldrew0619@gmail.com>
2023-06-05 10:43:27 +0500

    Improve UI responsiveness

--- dev/src/ui.py
+++ dev/src/ui.py
@@ -40,3 +40,4 @@
 # 2023-05-24T14:19:43  Fix layout shift on load  (rev 272)
 # 2023-05-25T13:37:38  Fix layout shift on load  (rev 276)
 # 2023-06-01T14:31:26  Improve mobile responsiveness  (rev 295)
+# 2023-06-05T10:43:27  Improve UI responsiveness  (rev 297)
