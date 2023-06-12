2efbc6bb58a47626c39576d92abad25a13e01051
Michael0619-D <michaeldrew0619@gmail.com>
2023-06-12 11:26:02 +0500

    Fix authentication bug

--- dev/src/auth.py
+++ dev/src/auth.py
@@ -42,3 +42,4 @@
 # 2023-05-26T20:19:07  Handle expired tokens on refresh  (rev 281)
 # 2023-05-31T11:31:05  Handle expired tokens on refresh  (rev 290)
 # 2023-06-01T14:27:45  Patch OAuth callback edge case  (rev 294)
+# 2023-06-12T11:26:02  Fix authentication bug  (rev 313)
