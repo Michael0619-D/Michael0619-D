e5e3c688362a8b2aff0dd293c33d4265717fdf44
Michael0619-D <michaeldrew0619@gmail.com>
2023-07-13 10:13:45 +0500

    Return structured error responses

--- dev/src/errors.py
+++ dev/src/errors.py
@@ -42,3 +42,4 @@
 # 2023-06-29T21:28:44  Improve error messages for validation  (rev 352)
 # 2023-07-01T11:20:17  Wrap external calls with retries  (rev 354)
 # 2023-07-01T20:18:22  Improve error messages for validation  (rev 355)
+# 2023-07-13T10:13:45  Return structured error responses  (rev 371)
