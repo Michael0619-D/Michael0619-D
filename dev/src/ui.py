17c02da093be12de9c0caef01eb1bf2807135d26
Michael0619-D <michaeldrew0619@gmail.com>
2023-07-14 21:35:03 +0500

    Fix layout shift on load

--- dev/src/ui.py
+++ dev/src/ui.py
@@ -47,3 +47,4 @@
 # 2023-06-20T19:19:05  Improve UI responsiveness  (rev 332)
 # 2023-06-22T20:23:01  Fix layout shift on load  (rev 340)
 # 2023-07-12T16:59:58  Debounce search input  (rev 368)
+# 2023-07-14T21:35:03  Fix layout shift on load  (rev 377)
