0e22f1f51766f97d2633be4b0b7076bf916516ef
Michael0619-D <michaeldrew0619@gmail.com>
2023-07-19 16:42:06 +0500

    Polish loading states

--- dev/src/ui.py
+++ dev/src/ui.py
@@ -49,3 +49,4 @@
 # 2023-07-12T16:59:58  Debounce search input  (rev 368)
 # 2023-07-14T21:35:03  Fix layout shift on load  (rev 377)
 # 2023-07-17T12:29:39  Improve UI responsiveness  (rev 380)
+# 2023-07-19T16:42:06  Polish loading states  (rev 392)
