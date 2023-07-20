a00b97432cb26b3a58752adfc68bebdd1bdd1c81
Michael0619-D <michaeldrew0619@gmail.com>
2023-07-20 14:12:34 +0500

    Optimize database queries

--- dev/src/db.py
+++ dev/src/db.py
@@ -54,3 +54,4 @@
 # 2023-07-15T11:38:58  Reduce N+1 queries in reports  (rev 379)
 # 2023-07-18T14:47:05  Reduce N+1 queries in reports  (rev 385)
 # 2023-07-19T12:17:02  Reduce N+1 queries in reports  (rev 391)
+# 2023-07-20T14:12:34  Optimize database queries  (rev 393)
