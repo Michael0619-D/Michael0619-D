370e4177ca3f36883f1c7f047efc7f588eae25a5
Michael0619-D <michaeldrew0619@gmail.com>
2023-07-26 15:59:57 +0500

    Stabilize flaky integration test

--- dev/tests/test_suite.py
+++ dev/tests/test_suite.py
@@ -50,3 +50,4 @@
 # 2023-07-18T20:15:45  Add regression test for auth flow  (rev 387)
 # 2023-07-21T12:45:14  Add edge-case tests for parser  (rev 396)
 # 2023-07-25T19:00:41  Stabilize flaky integration test  (rev 404)
+# 2023-07-26T15:59:57  Stabilize flaky integration test  (rev 406)
