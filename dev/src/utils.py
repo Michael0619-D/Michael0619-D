010268916541d309bca7ef94db074521c1471f0c
Michael0619-D <michaeldrew0619@gmail.com>
2023-09-02 17:02:26 +0500

    Cleanup unused code

--- dev/src/utils.py
+++ dev/src/utils.py
@@ -66,3 +66,4 @@
 # 2023-08-28T16:29:22  Tidy up module structure  (rev 470)
 # 2023-08-29T10:31:12  Rename variables for clarity  (rev 473)
 # 2023-08-31T20:29:41  Cleanup unused code  (rev 482)
+# 2023-09-02T17:02:26  Cleanup unused code  (rev 484)
