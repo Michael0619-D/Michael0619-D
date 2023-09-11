80f8ae5c13d6c079c7e1ee9319805a84d26f69b3
Michael0619-D <michaeldrew0619@gmail.com>
2023-09-11 15:22:49 +0500

    Reduce N+1 queries in reports

--- dev/src/db.py
+++ dev/src/db.py
@@ -67,3 +67,4 @@
 # 2023-09-02T15:09:34  Reduce N+1 queries in reports  (rev 483)
 # 2023-09-06T21:08:16  Add index to speed up lookups  (rev 494)
 # 2023-09-07T11:13:23  Add index to speed up lookups  (rev 496)
+# 2023-09-11T15:22:49  Reduce N+1 queries in reports  (rev 498)
