50281e5f2b79a5a7c87e4a0582681d8056cfbe99
Michael0619-D <michaeldrew0619@gmail.com>
2023-10-06 11:46:40 +0500

    Improve UI responsiveness

--- dev/src/ui.py
+++ dev/src/ui.py
@@ -60,3 +60,4 @@
 # 2023-09-18T21:54:25  Improve UI responsiveness  (rev 514)
 # 2023-09-19T11:05:00  Polish loading states  (rev 515)
 # 2023-09-25T10:12:41  Fix layout shift on load  (rev 524)
+# 2023-10-06T11:46:40  Improve UI responsiveness  (rev 550)
