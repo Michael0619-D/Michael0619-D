654d7c87cc13d5b3af2e8328b443100e96d1d24f
Michael0619-D <michaeldrew0619@gmail.com>
2023-10-10 18:54:54 +0500

    Add edge-case tests for parser

--- dev/tests/test_suite.py
+++ dev/tests/test_suite.py
@@ -71,3 +71,4 @@
 # 2023-10-05T18:14:33  Stabilize flaky integration test  (rev 548)
 # 2023-10-09T12:50:40  Stabilize flaky integration test  (rev 553)
 # 2023-10-10T10:38:30  Add unit tests  (rev 556)
+# 2023-10-10T18:54:54  Add edge-case tests for parser  (rev 558)
