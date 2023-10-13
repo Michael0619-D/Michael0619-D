f79cf03611fb9db36cac1ae054a47363ae32af38
Michael0619-D <michaeldrew0619@gmail.com>
2023-10-13 15:37:55 +0500

    Fix layout shift on load

--- dev/src/ui.py
+++ dev/src/ui.py
@@ -63,3 +63,4 @@
 # 2023-10-06T11:46:40  Improve UI responsiveness  (rev 550)
 # 2023-10-09T18:27:30  Polish loading states  (rev 555)
 # 2023-10-11T17:39:58  Improve UI responsiveness  (rev 559)
+# 2023-10-13T15:37:55  Fix layout shift on load  (rev 561)
