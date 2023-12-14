d5cdfddb16360340dc0df3aac1679a34518816b8
Michael0619-D <michaeldrew0619@gmail.com>
2023-12-14 20:27:44 +0500

    Fix session timeout handling

--- dev/src/auth.py
+++ dev/src/auth.py
@@ -79,3 +79,4 @@
 # 2023-11-30T14:29:41  Fix authentication bug  (rev 633)
 # 2023-12-12T15:05:00  Patch OAuth callback edge case  (rev 654)
 # 2023-12-12T18:36:34  Fix authentication bug  (rev 655)
+# 2023-12-14T20:27:44  Fix session timeout handling  (rev 660)
