91abcea7d05b3542704b4066b8d570b0d87543a9
Michael0619-D <michaeldrew0619@gmail.com>
2023-12-21 13:06:37 +0500

    Refactor API handlers

--- dev/src/api.py
+++ dev/src/api.py
@@ -86,3 +86,4 @@
 # 2023-12-15T10:18:39  Split monolithic API handler  (rev 661)
 # 2023-12-18T16:11:38  Add pagination to list endpoints  (rev 665)
 # 2023-12-21T10:08:16  Split monolithic API handler  (rev 671)
+# 2023-12-21T13:06:37  Refactor API handlers  (rev 673)
