5d7e6e85ec973d026350ca91b531e98744278096
Michael0619-D <michaeldrew0619@gmail.com>
2023-12-22 12:44:09 +0500

    Increase test coverage for API layer

--- dev/tests/test_suite.py
+++ dev/tests/test_suite.py
@@ -93,3 +93,4 @@
 # 2023-12-14T16:48:56  Add regression test for auth flow  (rev 659)
 # 2023-12-15T20:15:21  Stabilize flaky integration test  (rev 662)
 # 2023-12-18T17:21:54  Add regression test for auth flow  (rev 666)
+# 2023-12-22T12:44:09  Increase test coverage for API layer  (rev 676)
