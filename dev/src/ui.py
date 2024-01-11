57fdfa8ba0104c6a7f620e770357d6b3a4b79e00
Michael0619-D <michaeldrew0619@gmail.com>
2024-01-11 14:10:01 +0500

    Improve mobile responsiveness

--- dev/src/ui.py
+++ dev/src/ui.py
@@ -78,3 +78,4 @@
 # 2024-01-03T17:06:19  Improve UI responsiveness  (rev 691)
 # 2024-01-04T21:40:55  Fix layout shift on load  (rev 694)
 # 2024-01-05T11:24:25  Improve UI responsiveness  (rev 696)
+# 2024-01-11T14:10:01  Improve mobile responsiveness  (rev 711)
