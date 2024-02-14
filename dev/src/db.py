c8ad7c060444d99540218b368beb1f8b8efd51a0
Michael0619-D <michaeldrew0619@gmail.com>
2024-02-14 20:53:16 +0500

    Reduce N+1 queries in reports

--- dev/src/db.py
+++ dev/src/db.py
@@ -110,3 +110,4 @@
 # 2024-02-06T13:06:05  Cache frequent read queries  (rev 792)
 # 2024-02-13T15:29:36  Reduce N+1 queries in reports  (rev 817)
 # 2024-02-14T14:04:34  Batch database writes  (rev 820)
+# 2024-02-14T20:53:16  Reduce N+1 queries in reports  (rev 822)
