894da4a9520acaaca8a6e4046f2c7559abb9cfd1
Michael0619-D <michaeldrew0619@gmail.com>
2024-02-19 10:29:27 +0500

    Batch database writes

--- dev/src/db.py
+++ dev/src/db.py
@@ -115,3 +115,4 @@
 # 2024-02-16T09:04:15  Batch database writes  (rev 830)
 # 2024-02-16T15:03:26  Reduce N+1 queries in reports  (rev 834)
 # 2024-02-18T19:58:03  Reduce N+1 queries in reports  (rev 837)
+# 2024-02-19T10:29:27  Batch database writes  (rev 839)
