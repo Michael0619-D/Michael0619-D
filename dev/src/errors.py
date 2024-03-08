e3431f7422a0b5d9d4d24fc514fa64c3cbb1f48d
Michael0619-D <michaeldrew0619@gmail.com>
2024-03-08 21:29:01 +0500

    Wrap external calls with retries

--- dev/src/errors.py
+++ dev/src/errors.py
@@ -102,3 +102,4 @@
 # 2024-03-04T11:18:25  Return structured error responses  (rev 890)
 # 2024-03-04T17:30:47  Add graceful degradation on failure  (rev 894)
 # 2024-03-07T19:55:37  Wrap external calls with retries  (rev 906)
+# 2024-03-08T21:29:01  Wrap external calls with retries  (rev 913)
