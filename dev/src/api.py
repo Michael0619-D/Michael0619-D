34d1afa2712cc44b927b22ad12c7142a60113f0c
Michael0619-D <michaeldrew0619@gmail.com>
2024-04-29 11:33:07 +0500

    Simplify API response serialization

--- dev/src/api.py
+++ dev/src/api.py
@@ -126,3 +126,4 @@
 # 2024-04-25T21:12:53  Simplify API response serialization  (rev 1054)
 # 2024-04-26T12:36:17  Refactor API handlers  (rev 1056)
 # 2024-04-26T19:46:42  Simplify API response serialization  (rev 1061)
+# 2024-04-29T11:33:07  Simplify API response serialization  (rev 1067)
