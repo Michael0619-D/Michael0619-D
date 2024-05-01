ca104b818ab0eb3ba1b76bc1b96053718befbeb9
Michael0619-D <michaeldrew0619@gmail.com>
2024-05-01 13:30:23 +0500

    Reduce N+1 queries in reports

--- dev/src/db.py
+++ dev/src/db.py
@@ -140,3 +140,4 @@
 # 2024-04-25T17:08:32  Add index to speed up lookups  (rev 1052)
 # 2024-04-27T15:50:08  Batch database writes  (rev 1064)
 # 2024-04-30T09:22:11  Optimize database queries  (rev 1069)
+# 2024-05-01T13:30:23  Reduce N+1 queries in reports  (rev 1076)
