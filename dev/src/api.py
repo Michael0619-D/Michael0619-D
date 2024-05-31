b45854459139c125650a26bfbe5e65a0dedd7636
Michael0619-D <michaeldrew0619@gmail.com>
2024-05-31 09:35:59 +0500

    Add pagination to list endpoints

--- dev/src/api.py
+++ dev/src/api.py
@@ -135,3 +135,4 @@
 # 2024-05-22T18:37:56  Add pagination to list endpoints  (rev 1145)
 # 2024-05-27T09:15:49  Simplify API response serialization  (rev 1167)
 # 2024-05-30T10:56:19  Split monolithic API handler  (rev 1183)
+# 2024-05-31T09:35:59  Add pagination to list endpoints  (rev 1188)
