9a902167a6066fbfa54bd246578f22e1b712b02a
Michael0619-D <michaeldrew0619@gmail.com>
2024-06-09 13:31:02 +0500

    Refactor API handlers

--- dev/src/api.py
+++ dev/src/api.py
@@ -141,3 +141,4 @@
 # 2024-06-04T10:39:33  Refactor API handlers  (rev 1201)
 # 2024-06-04T12:52:04  Refactor API request routing  (rev 1203)
 # 2024-06-05T15:50:09  Add pagination to list endpoints  (rev 1211)
+# 2024-06-09T13:31:02  Refactor API handlers  (rev 1225)
