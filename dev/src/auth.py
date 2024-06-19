1a371ea5378245d7a37a0a8e36517d86cac2198c
Michael0619-D <michaeldrew0619@gmail.com>
2024-06-19 14:17:07 +0500

    Patch OAuth callback edge case

--- dev/src/auth.py
+++ dev/src/auth.py
@@ -164,3 +164,4 @@
 # 2024-06-16T09:30:55  Handle expired tokens on refresh  (rev 1257)
 # 2024-06-17T10:56:22  Patch OAuth callback edge case  (rev 1260)
 # 2024-06-19T11:49:39  Patch OAuth callback edge case  (rev 1269)
+# 2024-06-19T14:17:07  Patch OAuth callback edge case  (rev 1272)
