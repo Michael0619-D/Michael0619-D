68888985a0269560332cf2ffa1922cd6166aab84
Michael0619-D <michaeldrew0619@gmail.com>
2024-07-24 09:41:24 +0500

    Debounce search input

--- dev/src/ui.py
+++ dev/src/ui.py
@@ -166,3 +166,4 @@
 # 2024-07-15T15:47:54  Fix layout shift on load  (rev 1351)
 # 2024-07-16T16:42:30  Polish loading states  (rev 1356)
 # 2024-07-17T20:11:21  Polish loading states  (rev 1361)
+# 2024-07-24T09:41:24  Debounce search input  (rev 1382)
