31d4e59c49556d92553c98f3f19419494912b90f
Michael0619-D <michaeldrew0619@gmail.com>
2024-08-05 15:53:49 +0500

    Cleanup unused code

--- dev/src/utils.py
+++ dev/src/utils.py
@@ -189,3 +189,4 @@
 # 2024-08-02T15:45:01  Tidy up module structure  (rev 1423)
 # 2024-08-02T20:41:10  Rename variables for clarity  (rev 1426)
 # 2024-08-05T09:07:23  Rename variables for clarity  (rev 1428)
+# 2024-08-05T15:53:49  Cleanup unused code  (rev 1431)
