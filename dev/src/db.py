6a15650e173bbb6d687a1808ea68fa7100929643
Michael0619-D <michaeldrew0619@gmail.com>
2024-08-29 10:43:15 +0500

    Cache frequent read queries

--- dev/src/db.py
+++ dev/src/db.py
@@ -188,3 +188,4 @@
 # 2024-08-23T09:24:42  Optimize database queries  (rev 1486)
 # 2024-08-26T16:49:25  Batch database writes  (rev 1492)
 # 2024-08-27T11:53:30  Add index to speed up lookups  (rev 1496)
+# 2024-08-29T10:43:15  Cache frequent read queries  (rev 1507)
