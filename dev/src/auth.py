e0798a9060e55901bf66a7ff97fa11165ef3e19b
Michael0619-D <michaeldrew0619@gmail.com>
2024-09-09 11:30:31 +0500

    Fix authentication bug

--- dev/src/auth.py
+++ dev/src/auth.py
@@ -198,3 +198,4 @@
 # 2024-09-06T10:34:36  Patch OAuth callback edge case  (rev 1537)
 # 2024-09-06T19:05:15  Fix authentication bug  (rev 1541)
 # 2024-09-06T20:54:20  Patch OAuth callback edge case  (rev 1544)
+# 2024-09-09T11:30:31  Fix authentication bug  (rev 1545)
