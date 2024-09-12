6100d37a3a9d02323ae5eb3bc5b38261eb63eda3
Michael0619-D <michaeldrew0619@gmail.com>
2024-09-12 12:49:00 +0500

    Add edge-case tests for parser

--- dev/tests/test_suite.py
+++ dev/tests/test_suite.py
@@ -211,3 +211,4 @@
 # 2024-09-11T19:15:44  Add unit tests  (rev 1559)
 # 2024-09-11T19:47:26  Stabilize flaky integration test  (rev 1560)
 # 2024-09-12T10:59:39  Add regression test for auth flow  (rev 1565)
+# 2024-09-12T12:49:00  Add edge-case tests for parser  (rev 1566)
