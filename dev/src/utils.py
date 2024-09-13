9e9d14e3c528ebc25a90ea85dfcfad73d22a8285
Michael0619-D <michaeldrew0619@gmail.com>
2024-09-13 14:00:26 +0500

    Drop deprecated helpers

--- dev/src/utils.py
+++ dev/src/utils.py
@@ -212,3 +212,4 @@
 # 2024-09-05T12:27:16  Remove dead imports  (rev 1533)
 # 2024-09-05T14:43:07  Cleanup unused code  (rev 1534)
 # 2024-09-10T20:08:50  Cleanup unused code  (rev 1556)
+# 2024-09-13T14:00:26  Drop deprecated helpers  (rev 1572)
