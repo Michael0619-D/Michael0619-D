3c1606fdb09c5c94d6e699f0b0d4a949558abed6
Michael0619-D <michaeldrew0619@gmail.com>
2024-09-28 11:03:41 +0500

    Improve UI responsiveness

--- dev/src/ui.py
+++ dev/src/ui.py
@@ -187,3 +187,4 @@
 # 2024-09-23T10:19:05  Debounce search input  (rev 1602)
 # 2024-09-24T09:53:09  Fix layout shift on load  (rev 1610)
 # 2024-09-24T20:25:27  Debounce search input  (rev 1613)
+# 2024-09-28T11:03:41  Improve UI responsiveness  (rev 1632)
