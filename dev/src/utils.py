2ac467e0347ca687342d9a441080f8d181b3beb6
Michael0619-D <michaeldrew0619@gmail.com>
2024-10-09 15:52:31 +0500

    Drop deprecated helpers

--- dev/src/utils.py
+++ dev/src/utils.py
@@ -221,3 +221,4 @@
 # 2024-10-03T13:52:21  Cleanup unused code  (rev 1658)
 # 2024-10-07T19:49:37  Rename variables for clarity  (rev 1672)
 # 2024-10-07T21:04:39  Cleanup unused code  (rev 1673)
+# 2024-10-09T15:52:31  Drop deprecated helpers  (rev 1677)
