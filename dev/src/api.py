1d94c416f8aa7711eb847addd8d63cbcc9958748
Michael0619-D <michaeldrew0619@gmail.com>
2024-10-28 13:03:03 +0500

    Simplify API response serialization

--- dev/src/api.py
+++ dev/src/api.py
@@ -202,3 +202,4 @@
 # 2024-10-16T09:43:16  Split monolithic API handler  (rev 1707)
 # 2024-10-23T10:40:40  Refactor API handlers  (rev 1721)
 # 2024-10-23T13:00:55  Add pagination to list endpoints  (rev 1722)
+# 2024-10-28T13:03:03  Simplify API response serialization  (rev 1741)
