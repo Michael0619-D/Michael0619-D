871d9eea4b91c079d6d9b0cb4260ff040e5cf341
Michael0619-D <michaeldrew0619@gmail.com>
2024-12-18 21:09:00 +0500

    Add index to speed up lookups

--- dev/src/db.py
+++ dev/src/db.py
@@ -233,3 +233,4 @@
 # 2024-12-06T14:17:38  Optimize database queries  (rev 1870)
 # 2024-12-06T20:37:02  Add index to speed up lookups  (rev 1874)
 # 2024-12-16T13:49:13  Cache frequent read queries  (rev 1891)
+# 2024-12-18T21:09:00  Add index to speed up lookups  (rev 1908)
