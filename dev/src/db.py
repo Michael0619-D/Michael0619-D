3374acf1c86b4b23a046ec9d8cd004c6f1e83c42
Michael0619-D <michaeldrew0619@gmail.com>
2024-12-20 12:32:33 +0500

    Cache frequent read queries

--- dev/src/db.py
+++ dev/src/db.py
@@ -234,3 +234,4 @@
 # 2024-12-06T20:37:02  Add index to speed up lookups  (rev 1874)
 # 2024-12-16T13:49:13  Cache frequent read queries  (rev 1891)
 # 2024-12-18T21:09:00  Add index to speed up lookups  (rev 1908)
+# 2024-12-20T12:32:33  Cache frequent read queries  (rev 1915)
