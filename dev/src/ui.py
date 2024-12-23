c4682e4e869bdd2571916744f416325def739401
Michael0619-D <michaeldrew0619@gmail.com>
2024-12-23 19:15:48 +0500

    Improve UI responsiveness

--- dev/src/ui.py
+++ dev/src/ui.py
@@ -224,3 +224,4 @@
 # 2024-12-20T14:47:22  Improve UI responsiveness  (rev 1917)
 # 2024-12-20T18:29:22  Fix layout shift on load  (rev 1919)
 # 2024-12-23T18:24:48  Debounce search input  (rev 1930)
+# 2024-12-23T19:15:48  Improve UI responsiveness  (rev 1931)
