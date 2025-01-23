d3ed988c3f0c213e55e2028eb2075e22d95bd1d2
Michael0619-D <michaeldrew0619@gmail.com>
2025-01-23 20:24:28 +0500

    Improve UI responsiveness

--- dev/src/ui.py
+++ dev/src/ui.py
@@ -229,3 +229,4 @@
 # 2025-01-07T13:31:07  Polish loading states  (rev 1951)
 # 2025-01-08T19:47:12  Improve UI responsiveness  (rev 1958)
 # 2025-01-11T19:18:47  Debounce search input  (rev 1965)
+# 2025-01-23T20:24:28  Improve UI responsiveness  (rev 1987)
