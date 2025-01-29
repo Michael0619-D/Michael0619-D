2acda74b7575df6af3f39eb1ff8313b5ea0d970b
Michael0619-D <michaeldrew0619@gmail.com>
2025-01-29 11:50:52 +0500

    Stabilize flaky integration test

--- dev/tests/test_suite.py
+++ dev/tests/test_suite.py
@@ -268,3 +268,4 @@
 # 2025-01-27T19:39:10  Add unit tests  (rev 1990)
 # 2025-01-27T20:06:19  Add edge-case tests for parser  (rev 1991)
 # 2025-01-29T10:50:01  Stabilize flaky integration test  (rev 1996)
+# 2025-01-29T11:50:52  Stabilize flaky integration test  (rev 1997)
