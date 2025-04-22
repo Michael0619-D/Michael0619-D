61d2db1f0ad7a881e0776ba5fb72566ea5a19493
Michael0619-D <michaeldrew0619@gmail.com>
2025-04-22 19:30:22 +0500

    Stabilize flaky integration test

--- dev/tests/test_suite.py
+++ dev/tests/test_suite.py
@@ -297,3 +297,4 @@
 # 2025-04-17T15:40:46  Stabilize flaky integration test  (rev 2212)
 # 2025-04-18T12:09:05  Add edge-case tests for parser  (rev 2213)
 # 2025-04-21T19:41:37  Increase test coverage for API layer  (rev 2223)
+# 2025-04-22T19:30:22  Stabilize flaky integration test  (rev 2224)
