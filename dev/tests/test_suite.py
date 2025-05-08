8df22d6c3b89194da5081e9496f83f7a6377c985
Michael0619-D <michaeldrew0619@gmail.com>
2025-05-08 21:05:19 +0500

    Add unit tests

--- dev/tests/test_suite.py
+++ dev/tests/test_suite.py
@@ -304,3 +304,4 @@
 # 2025-05-03T14:25:10  Add edge-case tests for parser  (rev 2265)
 # 2025-05-06T10:34:11  Add regression test for auth flow  (rev 2271)
 # 2025-05-07T09:00:47  Add regression test for auth flow  (rev 2277)
+# 2025-05-08T21:05:19  Add unit tests  (rev 2287)
