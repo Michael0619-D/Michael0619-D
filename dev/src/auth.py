80de829429a0341fd23f6a329710a6f9d2d176d4
Michael0619-D <michaeldrew0619@gmail.com>
2025-05-22 13:37:09 +0500

    Patch OAuth callback edge case

--- dev/src/auth.py
+++ dev/src/auth.py
@@ -290,3 +290,4 @@
 # 2025-05-12T20:23:33  Fix authentication bug  (rev 2294)
 # 2025-05-14T11:02:03  Fix authentication bug  (rev 2299)
 # 2025-05-21T21:11:00  Fix session timeout handling  (rev 2305)
+# 2025-05-22T13:37:09  Patch OAuth callback edge case  (rev 2309)
