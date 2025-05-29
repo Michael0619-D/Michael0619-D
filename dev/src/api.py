d956b7b2f67cf25ec291287a99c767c2312724ab
Michael0619-D <michaeldrew0619@gmail.com>
2025-05-29 15:05:23 +0500

    Split monolithic API handler

--- dev/src/api.py
+++ dev/src/api.py
@@ -277,3 +277,4 @@
 # 2025-05-23T12:57:14  Simplify API response serialization  (rev 2316)
 # 2025-05-23T18:09:15  Refactor API handlers  (rev 2318)
 # 2025-05-23T18:11:19  Add pagination to list endpoints  (rev 2319)
+# 2025-05-29T15:05:23  Split monolithic API handler  (rev 2334)
