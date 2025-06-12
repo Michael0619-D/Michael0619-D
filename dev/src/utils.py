fbce5b8d1751a273ef775bccb207d5e704fa88d6
Michael0619-D <michaeldrew0619@gmail.com>
2025-06-12 19:30:46 +0500

    Rename variables for clarity

--- dev/src/utils.py
+++ dev/src/utils.py
@@ -311,3 +311,4 @@
 # 2025-06-04T16:46:40  Tidy up module structure  (rev 2345)
 # 2025-06-06T14:02:44  Remove dead imports  (rev 2349)
 # 2025-06-12T11:14:42  Cleanup unused code  (rev 2362)
+# 2025-06-12T19:30:46  Rename variables for clarity  (rev 2364)
