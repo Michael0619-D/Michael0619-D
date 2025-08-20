1d35a69e90bdbed49713a22ad0bc242e007bc0d6
Michael0619-D <michaeldrew0619@gmail.com>
2025-08-20 10:48:16 +0500

    Stabilize flaky integration test

--- dev/tests/test_suite.py
+++ dev/tests/test_suite.py
@@ -340,3 +340,4 @@
 # 2025-08-14T10:13:09  Add regression test for auth flow  (rev 2551)
 # 2025-08-18T09:56:21  Increase test coverage for API layer  (rev 2559)
 # 2025-08-19T13:30:37  Add unit tests  (rev 2563)
+# 2025-08-20T10:48:16  Stabilize flaky integration test  (rev 2568)
