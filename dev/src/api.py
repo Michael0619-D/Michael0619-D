7017b37c4e243c2debef91a7cd87f797d88909da
Michael0619-D <michaeldrew0619@gmail.com>
2025-09-22 20:43:58 +0500

    Refactor API request routing

--- dev/src/api.py
+++ dev/src/api.py
@@ -317,3 +317,4 @@
 # 2025-09-18T15:52:18  Add pagination to list endpoints  (rev 2649)
 # 2025-09-19T09:05:26  Split monolithic API handler  (rev 2650)
 # 2025-09-20T14:35:12  Add pagination to list endpoints  (rev 2657)
+# 2025-09-22T20:43:58  Refactor API request routing  (rev 2660)
