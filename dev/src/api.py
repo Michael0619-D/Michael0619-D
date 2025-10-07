a357318a872f96aaebb0b6385d999d28b10ef504
Michael0619-D <michaeldrew0619@gmail.com>
2025-10-07 18:08:01 +0500

    Refactor API handlers

--- dev/src/api.py
+++ dev/src/api.py
@@ -322,3 +322,4 @@
 # 2025-09-29T15:03:10  Refactor API request routing  (rev 2675)
 # 2025-10-05T16:56:09  Refactor API request routing  (rev 2696)
 # 2025-10-06T12:30:37  Refactor API request routing  (rev 2698)
+# 2025-10-07T18:08:01  Refactor API handlers  (rev 2702)
