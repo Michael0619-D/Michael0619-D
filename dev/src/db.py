6515b23f08316e6205dd40249493c227ba76edd2
Michael0619-D <michaeldrew0619@gmail.com>
2025-10-14 15:24:44 +0500

    Batch database writes

--- dev/src/db.py
+++ dev/src/db.py
@@ -344,3 +344,4 @@
 # 2025-10-07T09:26:17  Reduce N+1 queries in reports  (rev 2701)
 # 2025-10-10T15:13:11  Cache frequent read queries  (rev 2708)
 # 2025-10-14T14:54:45  Cache frequent read queries  (rev 2716)
+# 2025-10-14T15:24:44  Batch database writes  (rev 2717)
