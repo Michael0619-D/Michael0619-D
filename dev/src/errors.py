08d75c63463d825f723b047f3ad983040c24e944
Michael0619-D <michaeldrew0619@gmail.com>
2025-10-22 18:01:34 +0500

    Return structured error responses

--- dev/src/errors.py
+++ dev/src/errors.py
@@ -323,3 +323,4 @@
 # 2025-10-05T15:54:49  Add graceful degradation on failure  (rev 2695)
 # 2025-10-06T19:20:35  Wrap external calls with retries  (rev 2699)
 # 2025-10-06T20:30:01  Return structured error responses  (rev 2700)
+# 2025-10-22T18:01:34  Return structured error responses  (rev 2744)
