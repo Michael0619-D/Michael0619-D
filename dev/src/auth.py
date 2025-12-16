48076a2b2177be7bea9d6ab096151f89f80bf6ba
Michael0619-D <michaeldrew0619@gmail.com>
2025-12-16 19:11:42 +0500

    Patch OAuth callback edge case

--- dev/src/auth.py
+++ dev/src/auth.py
@@ -356,3 +356,4 @@
 # 2025-12-07T16:39:52  Fix authentication bug  (rev 2858)
 # 2025-12-10T20:19:13  Handle expired tokens on refresh  (rev 2863)
 # 2025-12-16T18:40:06  Patch OAuth callback edge case  (rev 2879)
+# 2025-12-16T19:11:42  Patch OAuth callback edge case  (rev 2880)
