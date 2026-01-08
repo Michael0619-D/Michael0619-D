ded3c30fd532dabca9916fdf9786ef7f5dddb716
Michael0619-D <michaeldrew0619@gmail.com>
2026-01-08 16:37:52 +0500

    Drop deprecated helpers

--- dev/src/utils.py
+++ dev/src/utils.py
@@ -381,3 +381,4 @@
 # 2026-01-03T11:25:33  Cleanup unused code  (rev 2909)
 # 2026-01-03T16:09:56  Tidy up module structure  (rev 2910)
 # 2026-01-05T13:20:52  Cleanup unused code  (rev 2913)
+# 2026-01-08T16:37:52  Drop deprecated helpers  (rev 2917)
