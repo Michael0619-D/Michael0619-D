da3fecb7f74c8223b4b4ade0825d52dfd2d68096
Michael0619-D <michaeldrew0619@gmail.com>
2026-02-24 10:19:58 +0500

    Cache frequent read queries

--- dev/src/db.py
+++ dev/src/db.py
@@ -387,3 +387,4 @@
 # 2026-02-12T11:59:24  Batch database writes  (rev 2975)
 # 2026-02-17T16:43:15  Reduce N+1 queries in reports  (rev 2981)
 # 2026-02-17T21:55:45  Cache frequent read queries  (rev 2982)
+# 2026-02-24T10:19:58  Cache frequent read queries  (rev 2991)
