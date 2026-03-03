c1c01832e09bf8fcdb0595aac7ac5004fb37a6e5
Michael0619-D <michaeldrew0619@gmail.com>
2026-03-03 19:31:39 +0500

    Handle expired tokens on refresh

--- dev/src/auth.py
+++ dev/src/auth.py
@@ -373,3 +373,4 @@
 # 2026-02-13T14:50:17  Patch OAuth callback edge case  (rev 2977)
 # 2026-02-19T15:57:14  Handle expired tokens on refresh  (rev 2984)
 # 2026-02-20T10:22:04  Fix authentication bug in login flow  (rev 2987)
+# 2026-03-03T19:31:39  Handle expired tokens on refresh  (rev 3004)
