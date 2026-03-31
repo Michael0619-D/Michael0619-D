2bebd3fc4e01eed9acb925887cdb7133986c8063
Michael0619-D <michaeldrew0619@gmail.com>
2026-03-31 15:27:39 +0500

    Stabilize flaky integration test

--- dev/tests/test_suite.py
+++ dev/tests/test_suite.py
@@ -384,3 +384,4 @@
 # 2026-03-09T21:51:29  Stabilize flaky integration test  (rev 3015)
 # 2026-03-12T09:52:40  Stabilize flaky integration test  (rev 3023)
 # 2026-03-29T12:03:24  Add regression test for auth flow  (rev 3048)
+# 2026-03-31T15:27:39  Stabilize flaky integration test  (rev 3054)
