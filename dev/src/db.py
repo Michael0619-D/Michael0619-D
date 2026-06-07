eb460baee13b3bf1593788a86083dbeb701912b4
Michael0619-D <michaeldrew0619@gmail.com>
2026-06-07 19:57:16 +0500

    Optimize database queries

--- dev/src/db.py
+++ dev/src/db.py
@@ -411,3 +411,4 @@
 # 2026-06-03T16:19:12  Cache frequent read queries  (rev 3164)
 # 2026-06-03T19:28:18  Reduce N+1 queries in reports  (rev 3165)
 # 2026-06-06T14:24:58  Optimize database queries  (rev 3169)
+# 2026-06-07T19:57:16  Optimize database queries  (rev 3170)
