fb8c747e5545940e89991422f6ffdd2b294517e0
Michael0619-D <michaeldrew0619@gmail.com>
2026-06-11 17:14:10 +0500

    Refactor API handlers

--- dev/src/api.py
+++ dev/src/api.py
@@ -374,3 +374,4 @@
 # 2026-06-05T19:13:35  Simplify API response serialization  (rev 3168)
 # 2026-06-09T18:33:19  Refactor API handlers  (rev 3177)
 # 2026-06-11T14:14:22  Split monolithic API handler  (rev 3180)
+# 2026-06-11T17:14:10  Refactor API handlers  (rev 3181)
