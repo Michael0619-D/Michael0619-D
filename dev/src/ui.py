29ad79f40647bcabfe9bc4b02f117fa47819c939
Michael0619-D <michaeldrew0619@gmail.com>
2026-07-01 15:47:10 +0500

    Improve mobile responsiveness

--- dev/src/ui.py
+++ dev/src/ui.py
@@ -397,3 +397,4 @@
 # 2026-06-22T18:46:32  Improve mobile responsiveness  (rev 3209)
 # 2026-06-25T09:12:00  Improve UI responsiveness  (rev 3215)
 # 2026-06-29T17:33:38  Debounce search input  (rev 3221)
+# 2026-07-01T15:47:10  Improve mobile responsiveness  (rev 3229)
